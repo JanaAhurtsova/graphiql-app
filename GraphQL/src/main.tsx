0f6ba2f7f0d18ac32a8fc774c8482ab7b04e3305
Jana Ahurtsova <byjankaaa@gmail.com>
2023-05-02 12:46:29 +0300

    feat: add footer and layout

--- GraphQL/src/main.tsx
+++ GraphQL/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app';
-import './index.css';
+import App from './components/app/App';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- GraphQL/vite.config.ts
+++ GraphQL/vite.config.ts
@@ -5,9 +5,17 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      components: `${path.resolve(__dirname, 'src/components/')}`,
+      pages: path.resolve(__dirname, 'src/pages'),
+      public: `${path.resolve(__dirname, 'public/')}`,
+      assets: `${path.resolve(__dirname, 'src/assets')}`,
+      managers: `${path.resolve(__dirname, 'src/managers')}`,
     },
   },
 });
