fbe1c9d86ec89b9c749b106f2f106d40bdb933c4
Vladimir Glazkov <vladimir.glazkov91@gmail.com>
2023-05-17 16:46:11 +0600

    fix: adaptive, remove artifacts, change login form input width

--- GraphQL/src/components/pages/welcomePage/WelcomePage.tsx
+++ GraphQL/src/components/pages/welcomePage/WelcomePage.tsx
@@ -25,12 +25,7 @@ export default function WelcomePage() {
   const yanaRef = useRef(null);
   const rsreactRef = useRef(null);
   const svetlanaRef = useRef(null);
-  const textRsRef = useRef(null);
-  const textReactRef = useRef(null);
-  const textGqRef = useRef(null);
-  const textSvetlanaRef = useRef(null);
-  const textYanaRef = useRef(null);
-  const textVladimirRef = useRef(null);
+  const pageContainerRef = useRef(null);
 
   useEffect(() => {
     if (ScrollTrigger.isTouch !== 1) {
@@ -55,26 +50,27 @@ export default function WelcomePage() {
           opacity: 0.2,
           scrollTrigger: {
             trigger: welcomeRef.current,
-            start: 'top',
+            start: 'bottom',
             end: '1500',
             scrub: true,
             invalidateOnRefresh: true,
           },
         }
       );
-      const itemsL = [graphqlRef, textRsRef, rsreactRef, yanaRef, textSvetlanaRef, vladimirRef];
+
+      const itemsL = [graphqlRef, rsreactRef, svetlanaRef];
 
       itemsL.forEach((work) => {
         gsap.fromTo(
           work.current,
-          { opacity: 0, x: -100 },
+          { opacity: 0, x: -150 },
           {
             opacity: 1,
             x: -5,
             scrollTrigger: {
               trigger: work.current,
               start: '-800',
-              end: '-200',
+              end: '500',
               scrub: true,
               invalidateOnRefresh: true,
             },
@@ -82,26 +78,19 @@ export default function WelcomePage() {
         );
       });
 
-      const itemsR = [
-        textGqRef,
-        rsschoolRef,
-        textReactRef,
-        textYanaRef,
-        svetlanaRef,
-        textVladimirRef,
-      ];
+      const itemsR = [rsschoolRef, yanaRef, vladimirRef];
 
       itemsR.forEach((work) => {
         gsap.fromTo(
           work.current,
-          { opacity: 0, x: 100 },
+          { opacity: 0, x: 150 },
           {
             opacity: 1,
             x: 5,
             scrollTrigger: {
               trigger: work.current,
               start: '-800',
-              end: '-200',
+              end: '500',
               scrub: true,
               invalidateOnRefresh: true,
             },
@@ -113,11 +102,11 @@ export default function WelcomePage() {
 
   return (
     <>
-      <section>
-        <div className="wrapper">
+      <section ref={pageContainerRef}>
+        <div className={styles.wrapper}>
           <div className={styles.content}>
             <header className={styles.hero_section}>
-              <img data-speed="0.9" className={styles.hero} src={hero} alt="Alt" ref={heroRef} />
+              <img data-speed="0.1" className={styles.hero} src={hero} alt="Alt" ref={heroRef} />
               <div>
                 <div data-speed=".5" className={styles.main_header} ref={welcomeRef}>
                   <h1 className={styles.main_title}>{langJSON[lang].welcomeText}</h1>
@@ -125,96 +114,61 @@ export default function WelcomePage() {
               </div>
             </header>
             <div className={styles.portfolio}>
-              <div className={styles.container}>
-                <main className={styles.gallery}>
-                  <div data-speed="0.9" className={styles.gallery__left}>
-                    <img
-                      className={styles.gallery__item}
-                      src={graphql}
-                      alt="Alt"
-                      ref={graphqlRef}
-                    />
-                    <div className={(styles.text_block, styles.gallery__item)} ref={textRsRef}>
-                      <h2 className={styles.text_block__h}>RS School</h2>
-                      <p className={styles.text_block__p}>{langJSON[lang].rsSchool}</p>
-                    </div>
-                    <img
-                      className={styles.gallery__item}
-                      src={rsreact}
-                      alt="Alt"
-                      ref={rsreactRef}
-                    />
-                  </div>
-
-                  <div data-speed="1" className={styles.gallery__right}>
-                    <div className={(styles.text_block, styles.gallery__item)} ref={textGqRef}>
-                      <h2 className={styles.text_block__h}>GraphQL</h2>
-                      <p className={styles.text_block__p}>{langJSON[lang].graphQl}</p>
-                    </div>
-
-                    <img
-                      className={styles.gallery__item}
-                      src={rsschool}
-                      alt="Alt"
-                      ref={rsschoolRef}
-                    />
-                    <div className={(styles.text_block, styles.gallery__item)} ref={textReactRef}>
-                      <h2 className={styles.text_block__h}>React Course</h2>
-                      <p className={styles.text_block__p}>{langJSON[lang].react}</p>
-                    </div>
-                  </div>
-                </main>
-                <h2 className={styles.team__h}>{langJSON[lang].team}</h2>
-                <div className={styles.gallery}>
-                  <div data-speed="0.9" className={styles.gallery__left}>
-                    <img className={styles.gallery__item} src={yana} alt="Alt" ref={yanaRef} />
-                    <div
-                      className={(styles.text_block, styles.gallery__item)}
-                      ref={textSvetlanaRef}
-                    >
-                      <h2 className={styles.text_block__h}>{langJSON[lang].svetlana}</h2>
-                      <p className={styles.text_block__p}>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam corrupti
-                        ut voluptatibus praesentium quae soluta at nesciunt recusandae itaque earum.
-                      </p>
-                    </div>
-                    <img
-                      className={styles.gallery__item}
-                      src={vladimir}
-                      alt="Alt"
-                      ref={vladimirRef}
-                    />
-                  </div>
-
-                  <div data-speed="1" className={styles.gallery__right}>
-                    <div className={(styles.text_block, styles.gallery__item)} ref={textYanaRef}>
-                      <h2 className={styles.text_block__h}>{langJSON[lang].jana}</h2>
-                      <p className={styles.text_block__p}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolores
-                        suscipit quam nostrum qui ut repellat vel ducimus cum enim.
-                      </p>
-                    </div>
-
-                    <img
-                      className={styles.gallery__item}
-                      src={svetlana}
-                      alt="Alt"
-                      ref={svetlanaRef}
-                    />
-                    <div
-                      className={(styles.text_block, styles.gallery__item)}
-                      ref={textVladimirRef}
-                    >
-                      <h2 className={styles.text_block__h}>{langJSON[lang].vladimir}</h2>
-                      <p className={styles.text_block__p}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, iusto.
-                        Dolore possimus maiores fugit provident sapiente impedit ipsum inventore
-                        quaerat.
-                      </p>
-                    </div>
+              <main className={styles.gallery}>
+                <div className={styles.gallery__item} ref={graphqlRef}>
+                  <img src={graphql} alt="Alt" className={styles.img} />
+                  <div className={styles.text_block}>
+                    <h2 className={styles.text_block__h}>GraphQL</h2>
+                    <p className={styles.text_block__p}>{langJSON[lang].graphQl}</p>
                   </div>
                 </div>
-              </div>
+                <div className={styles.gallery__item} ref={rsschoolRef}>
+                  <div className={styles.text_block}>
+                    <h2 className={styles.text_block__h}>RS School</h2>
+                    <p className={styles.text_block__p}>{langJSON[lang].rsSchool}</p>
+                  </div>
+                  <img src={rsschool} alt="Alt" className={styles.img} />
+                </div>
+                <div className={styles.gallery__item} ref={rsreactRef}>
+                  <img src={rsreact} alt="Alt" className={styles.img} />
+                  <div className={styles.text_block}>
+                    <h2 className={styles.text_block__h}>React Course</h2>
+                    <p className={styles.text_block__p}>{langJSON[lang].react}</p>
+                  </div>
+                </div>
+                <h2 className={styles.team__h}>{langJSON[lang].team}</h2>
+                <div className={styles.gallery__item} ref={yanaRef}>
+                  <div className={styles.text_block}>
+                    <h2 className={styles.text_block__h}>{langJSON[lang].jana}</h2>
+                    <p className={styles.text_block__p}>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolores
+                      suscipit quam nostrum qui ut repellat vel ducimus cum enim.
+                    </p>
+                  </div>
+                  <img src={yana} alt="Alt" className={styles.img} />
+                </div>
+                <div className={styles.gallery__item} ref={svetlanaRef}>
+                  <img src={svetlana} alt="Alt" className={styles.img} />
+                  <div className={styles.text_block}>
+                    <h2 className={styles.text_block__h}>{langJSON[lang].svetlana}</h2>
+                    <p className={styles.text_block__p}>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam corrupti ut
+                      voluptatibus praesentium quae soluta at nesciunt recusandae itaque earum.
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.gallery__item} ref={vladimirRef}>
+                  <div className={styles.text_block}>
+                    <h2 className={styles.text_block__h}>{langJSON[lang].vladimir}</h2>
+                    <p className={styles.text_block__p}>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, iusto.
+                      Dolore possimus maiores fugit provident sapiente impedit ipsum inventore
+                      quaerat.
+                    </p>
+                  </div>
+                  <img src={vladimir} alt="Alt" className={styles.img} />
+                </div>
+              </main>
             </div>
           </div>
         </div>
