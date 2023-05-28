a11a4063cba821990330e8f640004651830c2a31
Jana Ahurtsova <byjankaaa@gmail.com>
2023-05-28 19:07:41 +0300

    fix: fix remarks

--- GraphQL/cypress/e2e/404.spec.cy.ts
+++ GraphQL/cypress/e2e/404.spec.cy.ts
@@ -1,16 +1,13 @@
 /// <reference types="cypress" />
+import text from '../../src/assets/json/localization.json';
 
 describe('check 404 page', () => {
   it('show error page', () => {
     cy.visit('/test');
 
-    cy.contains('Nothing to see here');
+    cy.contains(text.en.errorTitle);
 
     cy.get('.ant-switch-inner').click();
-    cy.contains('Здесь ничего нет');
-  });
-
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+    cy.contains(text.ru.errorTitle);
   });
 });
